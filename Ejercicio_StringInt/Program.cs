c528d15e5bd1f881303535722ecc2d1cae1c9155
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-03-02 19:24:00 -0800

    Se creó Ejercicio_StringInt donde se pide nombre y edad.

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String edad;
+
+            //Pedir por consola el nombre de una persona y la edad
+            Console.WriteLine("Por favor, escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            edad = Console.ReadLine();
+
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad + " años.");
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
