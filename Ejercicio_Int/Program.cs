c92e38c73e8588862e6101e6921bf9ffd8138036
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-03-02 18:47:11 -0800

    Se creó Ejercicio_Int para realización de suma

---
+++ Ejercicio_Int/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.- Declarar tres variables de tipo INT
+            int numero_1 = 5;
+            int numero_2 = 20;
+            int numero_3 = 10;
+
+            //2.- Crear una variable de tipo INT la cual guardará la suma de las tres variables
+            int suma = numero_1 + numero_2 + numero_3;
+
+            //3.- Mostrar en pantalla el valor de SUMA "El valor de suma es:"
+            Console.WriteLine("El valor de la suma es: " + suma);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
