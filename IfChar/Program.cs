990d474009d63d20af8f1121d189c2fd3e42d426
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-03-18 18:48:05 -0700

    Practica IfChar(Ingresar un caracter)

---
+++ IfChar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un caracter
+            Console.WriteLine("Ingresa un cáracter:");
+            char c = (char)Console.Read();
+            if (Char.IsLetter(c))
+            {
+                if (char.IsLower(c))
+                {
+                    Console.WriteLine("El caracter es letra minuscula");
+                }
+                else
+                {
+                    Console.WriteLine("El caracter es letra mayuscula");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El cara ter es un numero");
+            }
+        
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
